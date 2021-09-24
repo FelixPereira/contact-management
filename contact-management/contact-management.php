cffa8e99b5abf7b0673d694eaa963210bef35ac2
Félix Pereira <49062313+FelixPereira@users.noreply.github.com>
2021-09-24 10:43:27 +0200

    Initial commit

---
+++ contact-management/contact-management.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Plugin Name: Contact Management Plugin
+ * Plugin URI: https://github.com/FelixPereira/contact-management
+ * Description: Wordpress plugin to manage contacts
+ * Author: Félix Pereira
+ * Author URI: https://github.com/FelixPereira/
+ * Version: 1.0.0
+ * Copyright (C) 2021 | Félix Pereira
+*/
